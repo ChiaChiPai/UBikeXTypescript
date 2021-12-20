405985e03e566fb80803ec1a451561808d046559
White Pai <white_pai@appdet.com>
2021-12-20 15:29:56 +0800

    feat: init map

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import L from 'leaflet'
+import mapConfig from './map.config'
+
+const {
+  coordinate,
+  zoomLevel,
+  tileLayerURL,
+  containerID
+} = mapConfig
+
+const taipeiCoord = coordinate
+
+const zoom = zoomLevel
+
+const map = L.map(containerID)
+
+map.setView(taipeiCoord, zoom)
+
+L.tileLayer(tileLayerURL).addTo(map);
