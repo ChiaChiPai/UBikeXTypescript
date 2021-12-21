120f77e0dffd948ae07ebf0222d0dd7f08de2b35
White Pai <white_pai@appdet.com>
2021-12-21 12:29:38 +0800

    feat: ser tooltip on map

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import L from 'leaflet'
+import L, { LayerGroup } from 'leaflet'
 import mapConfig from './map.config'
 import fetchData from './fetchData'
+import { districts } from './districtData'
+import { district } from './data'
 
+/* initialize */
 const {
   coordinate,
   zoomLevel,
@@ -19,6 +22,73 @@ map.setView(taipeiCoord, zoom)
 
 L.tileLayer(tileLayerURL).addTo(map);
 
-fetchData().then(data => {
-  console.log(data)
+/* 建立行政區 option DOM */
+// 選取 <select> 標籤
+const $selectDistrict = <HTMLSelectElement | null>(document.querySelector('#select-district'))
+
+if($selectDistrict === null) {
+  throw new Error('No select-district field was provided')
+}
+
+districts.forEach((d) => {
+  const $optionTag = document.createElement('option')
+
+  $optionTag.setAttribute('value', d)
+  $optionTag.innerText = d
+
+  $selectDistrict.appendChild($optionTag)
 })
+
+/* 繪製點位 */
+// 取出目前行政區
+let currentDistrict = $selectDistrict.value as district
+let markerLayer: LayerGroup
+
+function updateUBikeMap(district: district): void {
+  
+  // 轉換點位並繪製
+  fetchData().then(data => {
+    // 1. 將資料根據選到的行政區進行過濾
+    const selectData = data.filter(info => info.region === currentDistrict)
+  
+    // 2. 將 selectedData 裡面的 UBikeInfo 轉換成 Leaflet Marker
+    const markers = selectData.map(data => {
+      // 使用 L.Marker 裡面填入 LatLngExpression
+      const marker = new L.Marker(data.latLng)
+  
+      marker.bindTooltip(`
+        <p>${data.region} - ${data.stopName}</p>
+        <p>總自行車數: ${ data.totalBikes }</p>
+        <p>可用自行車數: ${ data.availableBikes }</p>
+      `)
+  
+      marker.on('mouseover', () => {
+        marker.openTooltip()
+      })
+  
+      marker.on('mouseleave', () => {
+        marker.closeTooltip()
+      })
+  
+      return marker
+  
+    })
+  
+    markerLayer = L.layerGroup(markers)
+    markerLayer.addTo(map)
+  })
+}
+
+updateUBikeMap(currentDistrict)
+
+/* 不同行政區渲染出不同的狀況 */
+$selectDistrict.addEventListener('change', (event) => {
+
+  let { value } = event.target as HTMLSelectElement
+  currentDistrict = value as district
+
+  markerLayer.remove()
+
+  updateUBikeMap(currentDistrict)
+})
+
